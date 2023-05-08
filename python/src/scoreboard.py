0618f8b2f6c54fc9a5a1cffdd210c4f35f0f8c82
emily <emily@bacheconsulting.com>
2023-05-08 10:26:40 +0200

    slice strategy in python

--- python/src/scoreboard.py
+++ python/src/scoreboard.py
@@ -9,17 +9,21 @@ def print_scoreboard():
     print_scoreboard_at(now)
 
 
-def print_scoreboard_at(now):
+def do_update_selection():
+    scorer.update_selection()
+
+
+def print_scoreboard_at(now, selection=do_update_selection):
     names = ["Steve", "Julie", "Francis"]
     print(f"Scoreboard at time {now}")
     for name in names:
-        print_score(name)
+        print_score(name, selection)
 
 
-def print_score(name):
+def print_score(name, selection=do_update_selection):
     if name == "Steve":
         scorer.flavour = IceCream.Strawberry
     else:
-        scorer.update_selection()
+        selection()
     score = scorer.get_score()
     print(f"{name} score: {score}")
--- python/tests/test_scoreboard.py
+++ python/tests/test_scoreboard.py
@@ -2,10 +2,15 @@ import datetime
 
 import approvaltests
 
-from scoreboard import print_scoreboard_at
+from scoreboard import print_scoreboard_at, do_update_selection
+
+
+def stub_update_selection():
+    pass
 
 
 def test_scoreboard(capsys):
-    print_scoreboard_at(datetime.datetime.fromisoformat("2023-05-08 10:12:37.023834"))
+    print_scoreboard_at(datetime.datetime.fromisoformat("2023-05-08 10:12:37.023834"),
+                        selection=stub_update_selection)
     captured = capsys.readouterr()
     approvaltests.verify(captured.out)
