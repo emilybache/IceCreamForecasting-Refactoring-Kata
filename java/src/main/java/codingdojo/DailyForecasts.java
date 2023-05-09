2109ba5752671c0f9a10f8375bb6277b37acc052
emily <emily@bacheconsulting.com>
2023-05-09 14:43:48 +0200

    more code in java version

---
+++ java/src/main/java/codingdojo/DailyForecasts.java
@@ -0,0 +1,32 @@
+package codingdojo;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class DailyForecasts {
+    private final Scorer scorer = new Scorer();
+
+
+
+    public void printSalesForecasts() {
+        var names = List.of( "Steve", "Julie", "Francis");
+        System.out.println("Forecast at: " + Instant.now());
+
+        for (String name: names) {
+            if (Objects.equals(name, "Steve"))
+            {
+                Scorer.flavour = IceCream.Strawberry;
+            }
+            else
+            {
+                scorer.updateSelection();
+            }
+            int score = scorer.getScore();
+            System.out.println(name + " score: " + score);
+        }
+    }
+
+
+}
