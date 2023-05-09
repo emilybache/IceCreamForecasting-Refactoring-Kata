98aefa7fbb9c23e0df037bb50dde3c2a4db95dd6
emily <emily@bacheconsulting.com>
2023-05-09 15:37:55 +0200

    first approval test

--- java/src/test/java/codingdojo/LongRangeForecastsTest.java
+++ java/src/test/java/codingdojo/LongRangeForecastsTest.java
@@ -1,7 +1,10 @@
 package codingdojo;
 
+import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,12 +13,25 @@ public class LongRangeForecastsTest {
 
     @Test
     void longRangeForecast() {
-        var expected = Map.of(
-                IceCream.Strawberry, 0,
-                IceCream.Vanilla, 0,
-                IceCream.Chocolate, 0
-        );
-        var actual = new LongRangeForecasts().longRangeForecast("Q3");
-        assertEquals(expected, actual);
+        var quarter = "Q3";
+        var actual = new LongRangeForecasts().longRangeForecast(quarter);
+        StringBuilder printedResult = printIceCreamForecast(actual);
+
+        Approvals.verify(printedResult);
+    }
+
+    private static StringBuilder printIceCreamForecast(Map<IceCream, Integer> actual) {
+        var printedResult = new StringBuilder();
+        printedResult.append("{");
+        var sortedKeys = new ArrayList<IceCream>(actual.keySet());
+        Collections.sort(sortedKeys);
+        for (IceCream key : sortedKeys) {
+            printedResult.append(key);
+            printedResult.append(":");
+            printedResult.append(actual.get(key));
+            printedResult.append(", ");
+        }
+        printedResult.append("}");
+        return printedResult;
     }
 }
