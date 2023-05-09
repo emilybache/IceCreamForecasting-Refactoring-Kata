4e13beb7bc63c16afc0af21e9bd6764734c3cea3
emily <emily@bacheconsulting.com>
2023-05-09 16:14:29 +0200

    use combination approvals

--- java/src/test/java/codingdojo/LongRangeForecastsTest.java
+++ java/src/test/java/codingdojo/LongRangeForecastsTest.java
@@ -1,6 +1,7 @@
 package codingdojo;
 
 import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
@@ -13,17 +14,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LongRangeForecastsTest {
 
-    @Test
-    void longRangeForecast() {
-        var quarter = "Q3";
+    String calculateLongRangeForecast(String quarter,
+                                                     Boolean weather1,
+                                                     Boolean weather2,
+                                                     Boolean weather3,
+                                                     Boolean weather4) {
+        var weatherMap = Map.of(
+                Instant.parse("2023-05-01T14:00:00.000-07:00"), weather1,
+                Instant.parse("2023-05-18T14:00:00.000-07:00"), weather2,
+                Instant.parse("2023-06-05T14:00:00.000-07:00"), weather3,
+                Instant.parse("2023-06-23T14:00:00.000-07:00"), weather4
+        );
+
+        var weather = new Boolean[] {true, true, false, false};
         Function<Instant, Boolean> goodWeather = interestingDate1 -> {
-            return true;
+            return weatherMap.getOrDefault(interestingDate1, true);
         };
 
         var actual = LongRangeForecasts.longRangeForecastWithScorerLookup(quarter, goodWeather);
         StringBuilder printedResult = printIceCreamForecast(actual);
+        return printedResult.toString();
+    }
 
-        Approvals.verify(printedResult);
+    @Test
+    void longRangeForecast() {
+        var quarters = new String[]{"Q1", "Q2", "Q3", "Q4"};
+        var weather1s = new Boolean[]{true, false};
+        var weather2s = new Boolean[]{true, false};
+        var weather3s = new Boolean[]{true, false};
+        var weather4s = new Boolean[]{true, false};
+
+
+        CombinationApprovals.verifyBestCoveringPairs(
+                this::calculateLongRangeForecast,
+                quarters,
+                weather1s,
+                weather2s,
+                weather3s,
+                weather4s
+        );
     }
 
     private static StringBuilder printIceCreamForecast(Map<IceCream, Integer> actual) {
