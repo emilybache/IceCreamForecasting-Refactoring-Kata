de84ba37e6051a05a4b96326b1c2e15ea5cd1bb1
emily <emily@bacheconsulting.com>
2023-05-09 15:23:19 +0200

    sample peel for Scorer

--- java/src/test/java/codingdojo/ScorerTest.java
+++ java/src/test/java/codingdojo/ScorerTest.java
@@ -1,14 +1,25 @@
 package codingdojo;
 
+import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.core.Options;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScorerTest {
+
+
     @Test
     void getScore() {
-        var scorer = new Scorer();
-        assertEquals(-1, scorer.getScore());
+
+        var weathers = new Boolean[]{true, false};
+        var flavours = new IceCream[]{IceCream.Strawberry, IceCream.Chocolate, IceCream.Vanilla, null};
+
+        CombinationApprovals.verifyAllCombinations(
+                Scorer::getScoreWithWeather,
+                weathers,
+                flavours
+        );
     }
 
 }
